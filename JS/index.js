101ce2ee17e0ea8523ec118c4aedfed271d4df19
Vusal Ismayilov <woosal@protonmail.com>
2021-03-27 11:54:48 +0300

    1.0

---
+++ JS/index.js
@@ -0,0 +1,104 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fetch = require('node-fetch');
+
+client.login("");
+
+client.on("ready", () => {
+    console.log("Let's Joke it up!");
+});
+
+client.on("message", msg => {
+
+    if (msg.channel.id === "825150249797419028" || msg.channel.id === "825290843614609448") {
+        if (msg.content.toLowerCase() === "djoker dark") {
+            fetch('https://v2.jokeapi.dev/joke/dark', {
+                method: 'GET',
+            })
+                .then(res => res.json())
+                .then(json => {
+                    if (json.type === "single") {
+                        msg.channel.send(json.joke);
+                    } else {
+                        msg.channel.send(json.setup + "\n" + json.delivery);
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+
+        if (msg.content.toLowerCase() === "djoker programming") {
+            fetch('https://v2.jokeapi.dev/joke/programming', {
+                method: 'GET',
+            })
+                .then(res => res.json())
+                .then(json => {
+                    if (json.type === "single") {
+                        msg.channel.send(json.joke);
+                    } else {
+                        msg.channel.send(json.setup + "\n" + json.delivery);
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+
+        if (msg.content.toLowerCase() === "djoker misc") {
+            fetch('https://v2.jokeapi.dev/joke/misc', {
+                method: 'GET',
+            })
+                .then(res => res.json())
+                .then(json => {
+                    if (json.type === "single") {
+                        msg.channel.send(json.joke);
+                    } else {
+                        msg.channel.send(json.setup + "\n" + json.delivery);
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+
+        if (msg.content.toLowerCase() === "djoker pun") {
+            fetch('https://v2.jokeapi.dev/joke/programming', {
+                method: 'GET',
+            })
+                .then(res => res.json())
+                .then(json => {
+                    if (json.type === "single") {
+                        msg.channel.send(json.joke);
+                    } else {
+                        msg.channel.send(json.setup + "\n" + json.delivery);
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+
+        if (msg.content.toLowerCase() === "djoker spooky") {
+            fetch('https://v2.jokeapi.dev/joke/spooky', {
+                method: 'GET',
+            })
+                .then(res => res.json())
+                .then(json => {
+                    if (json.type === "single") {
+                        msg.channel.send(json.joke);
+                    } else {
+                        msg.channel.send(json.setup + "\n" + json.delivery);
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+
+        if (msg.content.toLowerCase() === "djoker christmas") {
+            fetch('https://v2.jokeapi.dev/joke/christmas', {
+                method: 'GET',
+            })
+                .then(res => res.json())
+                .then(json => {
+                    if (json.type === "single") {
+                        msg.channel.send(json.joke);
+                    } else {
+                        msg.channel.send(json.setup + "\n" + json.delivery);
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+    }
+})
